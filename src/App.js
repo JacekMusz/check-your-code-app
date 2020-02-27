c1901e24450b63d9762b3f9b18f7c3f76245dec7
Jacek <muszynskijk@gmail.com>
2020-02-27 14:29:52 +0100

    Api methods

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import {
+  apiFilters,
+  apiCreateBundle,
+  apiCheckBundle,
+  apiGetAnalysis,
+  apiGetDiffAnalysis
+} from "./api/apiBundle";
 
 function App() {
-  return <div className="App"></div>;
+  let handleApiFilters = () => {
+    apiFilters();
+  };
+  let handleCreateBundle = () => {
+    apiCreateBundle();
+  };
+  let handleCheckBundle = () => {
+    apiCheckBundle(
+      "gh/JacekMusz/DEEPCODE_PRIVATE_BUNDLE/0ffcd1f224a3667853052b78e2e57e5328d9972b96f50d015d9d056a2dadb233"
+    );
+  };
+  let handleGetAnalysis = () => {
+    apiGetAnalysis(
+      "gh/JacekMusz/DEEPCODE_PRIVATE_BUNDLE/0ffcd1f224a3667853052b78e2e57e5328d9972b96f50d015d9d056a2dadb233"
+    );
+  };
+  let handleGetDiffAnalysis = () => {
+    apiGetDiffAnalysis(
+      "gh/JacekMusz/DEEPCODE_PRIVATE_BUNDLE/0ffcd1f224a3667853052b78e2e57e5328d9972b96f50d015d9d056a2dadb233"
+    );
+  };
+  return (
+    <div className="App">
+      <button onClick={() => handleApiFilters()}>Get Filters</button>
+      <button onClick={() => handleCreateBundle()}>CreateBundle</button>
+      <button onClick={() => handleCheckBundle()}>CheckBundle</button>
+      <button onClick={() => handleGetAnalysis()}>Get Analysis</button>
+      <button onClick={() => handleGetDiffAnalysis()}>Get Diff Analysis</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/api/apiBundle.js
@@ -0,0 +1,82 @@
+import { publicApi } from "./apiIndex";
+
+let kodJS =
+  "function clean(toClean, source){if (typeof(toClean) !== 'string') return true;if (typeof(source) !== 'string') return true;return source.replace(toClean, String('CLEANED')).toString();}";
+let kodJS2 = "true=false";
+export const apiFilters = () => {
+  return publicApi
+    .get("filters", {
+      headers: {
+        "Session-Token":
+          "08406328ef4d4bd1c986fd972eb10e83b44e21a9e9d768644c48426735d6638f"
+      }
+    })
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err));
+};
+export const apiCreateBundle = () => {
+  return publicApi
+    .post("bundle", {
+      "Content-Type": "application/json",
+      headers: {
+        "Session-Token":
+          "08406328ef4d4bd1c986fd972eb10e83b44e21a9e9d768644c48426735d6638f"
+      },
+      files: [{ filePath: "/file.js", fileContent: kodJS2 }]
+    })
+    .then(resp => console.log(resp.data.bundleId))
+    .catch(err => console.log(err));
+};
+
+export const apiCheckBundle = bundleID => {
+  console.log(bundleID);
+  return publicApi
+    .get(`bundle/${bundleID}`, {
+      "Content-Type": "application/json",
+      headers: {
+        "Session-Token":
+          "08406328ef4d4bd1c986fd972eb10e83b44e21a9e9d768644c48426735d6638f"
+      }
+    })
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err));
+};
+
+export const apiExtendBundle = () => {
+  return publicApi
+    .put(`bundle/:bundleId`, {
+      "Content-Type": "application/json",
+      headers: {
+        "Session-Token":
+          "08406328ef4d4bd1c986fd972eb10e83b44e21a9e9d768644c48426735d6638f"
+      },
+      files: [{ filePath: "/file2.js", fileContent: "const a = 3" }],
+      removedFiles: ["/file.js"]
+    })
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err));
+};
+
+export const apiGetAnalysis = bundleId => {
+  return publicApi
+    .get(`analysis/${bundleId}`, {
+      headers: {
+        "Session-Token":
+          "08406328ef4d4bd1c986fd972eb10e83b44e21a9e9d768644c48426735d6638f"
+      }
+    })
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err));
+};
+
+export const apiGetDiffAnalysis = bundleId => {
+  return publicApi
+    .get(`analysis/${bundleId}/${bundleId}`, {
+      headers: {
+        "Session-Token":
+          "08406328ef4d4bd1c986fd972eb10e83b44e21a9e9d768644c48426735d6638f"
+      }
+    })
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err));
+};
