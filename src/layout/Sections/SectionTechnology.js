fc30b8534b90d08627c186bb330f95f866d4e73c
Jacek <muszynskijk@gmail.com>
2020-04-22 20:24:57 +0200

    animation in SectionAuthor added

--- src/layout/Sections/SectionTechnology.js
+++ src/layout/Sections/SectionTechnology.js
@@ -6,8 +6,9 @@ import SectionTechnologyFooter from "./SectionTechnologyFooter.js";
 
 const SectionTechnology = () => {
   //-----REACT INTERSECTION OBSERVER-----
-  const [sectionTechnology, inView, entry] = useInView({
+  const [sectionTechnology, inView] = useInView({
     threshold: 0,
+    rootMargin: "-200px",
   });
   const [
     stopObserveSectionTechnology,
@@ -22,26 +23,55 @@ const SectionTechnology = () => {
   const article = useRef(null);
   const logo = useRef(null);
 
+  const myDuration = 1;
+
   useEffect(() => {
     if (inView && !stopObserveSectionTechnology) {
       // In order to stop listeining after first animation
       //
       //----ANIMATION ARTICLE AND ITS BACKGORUNDS-----
-      const tl = gsap.timeline({ defaults: { duration: 0.8 } });
-      tl.to(sectionBackgroundTwo.current, { opacity: 1, y: "2vh", x: "2vw" })
+      const tl = gsap.timeline({ defaults: { duration: myDuration } });
+      tl.to(sectionBackgroundTwo.current, { opacity: 1, y: "1vh", x: "4vw" })
         .to(
           sectionBackgroundOne.current,
-          { opacity: 1, y: "5vh", x: "6vw" },
-          "-=0.4"
+          { opacity: 1, y: "4vh", x: "7vw" },
+          `-=${myDuration * 0.8}`
         )
-        .to(article.current, { opacity: 1, y: "8vh", x: "10vw" }, "-=0.4")
-        .to([...article.current.children], { opacity: 1, y: "-20" }, "+=0.4")
-        .to(logoBackgroundOne.current, { opacity: 1, x: 0, y: 0 }, "-=1")
-        .to(logoBackgroundTwo.current, { opacity: 1, x: 0, y: 0 }, "-=0.5")
+        .to(
+          article.current,
+          { opacity: 1, y: "8vh", x: "10vw" },
+          `-=${myDuration * 0.8}`
+        )
+        .to(
+          [...article.current.children],
+          { opacity: 1, y: "-20" },
+          `-=${myDuration * 0.5}`
+        )
+        .to(
+          logoBackgroundOne.current,
+          { opacity: 1, x: 0, y: 0 },
+          `-=${myDuration * 0.4}`
+        )
+        .to(
+          logoBackgroundTwo.current,
+          { opacity: 1, x: 0, y: 0 },
+          `-=${myDuration * 0.6}`
+        )
         .to(
           logo.current,
-          { duration: 1.2, opacity: 1, x: "-15px", y: 0 },
-          "-=0.3"
+          { duration: 1.5, opacity: 1, x: "-20px", y: 0 },
+          `-=${myDuration * 0.6}`
+        )
+        .add("hideBackgrounds")
+        .to(
+          [logoBackgroundOne.current, logoBackgroundTwo.current],
+          { duration: myDuration * 0.7, opacity: 0, x: "-15px", y: "5px" },
+          "hideBackgrounds"
+        )
+        .to(
+          [sectionBackgroundTwo.current, sectionBackgroundOne.current],
+          { y: "8vh", x: "10vw", opacity: 0, duration: myDuration * 0.7 },
+          "hideBackgrounds"
         )
         .eventCallback("onComplete", setStopObserveSectionTechnology(1));
     }
