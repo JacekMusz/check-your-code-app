e3dbace1a2c9ad6209e021519035b3c735cffeca
Jacek <muszynskijk@gmail.com>
2020-05-26 01:44:03 +0200

    too  small Screen Notification added, index.html fixes, animation-fixes in section technology

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import NavBar from "./layout/NavBar";
 import { Route, Switch } from "react-router-dom";
 import PageMain from "./layout/PageMain";
 import PageEditor from "./layout/PageEditor";
+import ToSmallScreenNotification from "./layout/ToSmallScreenNotification";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <NavBar />
-        <Switch>
-          <Route path="/" exact component={PageMain} />
-          <Route path="/editor" component={PageEditor} />
-        </Switch>
-      </div>
+      <>
+        <div className="app">
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={PageMain} />
+            <Route path="/editor" component={PageEditor} />
+          </Switch>
+        </div>
+        <ToSmallScreenNotification />
+      </>
     );
   }
 }
---
+++ src/layout/ToSmallScreenNotification.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TosmallScreenNotification = () => {
+  return (
+    <div className="to-small-screen-notification">
+      <h3 className="to-small-screen-notification__title">
+        Please use bigger device (min. 1200px width and min. 600px height)
+      </h3>
+    </div>
+  );
+};
+
+export default TosmallScreenNotification;
